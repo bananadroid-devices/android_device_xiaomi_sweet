1d7be53636af65fa1dba1eb290b07fe5bd573e66
unsatifsed27 <unsatifsederror@gmail.com>
2023-04-19 14:54:11 +0700

    sweet: Banana Bringup

--- banana_sweet.mk
+++ banana_sweet.mk
@@ -11,16 +11,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common BananaDroid stuff.
+$(call inherit-product, vendor/banana/config/common.mk)
 
-PRODUCT_NAME := lineage_sweet
+PRODUCT_NAME := banana_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Banana Ify
+BANANA_MAINTAINER := Djampt
+BANANA_BUILD_TYPE := OFFICIAL
 
 # Face Unlock
 TARGET_FACE_UNLOCK_SUPPORTED := true
