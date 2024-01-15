0632dce6eb81ffb394d97f43d8acd0533da350e9
unsatifsed27 <unsatifsederror@gmail.com>
2024-01-15 04:12:34 +0100

    sweet: banana bringup

--- banana_sweet.mk
+++ banana_sweet.mk
@@ -11,10 +11,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common BananaDroid stuff.
+BANANA_BUILD_TYPE := OFFICIAL
+TARGET_EXCLUDE_MATLOG := true
+$(call inherit-product, vendor/banana/config/common.mk)
 
-PRODUCT_NAME := lineage_sweet
+PRODUCT_NAME := banana_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
